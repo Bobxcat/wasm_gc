325b5ad2a28375bf67b468aead36cf3ad2fe63ce
Bobxcat <ryan@rscheme.org>
2024-04-25 08:24:18 -0500

    Switched to HashMap for storing GcAlloc data, no more miri bugs

--- gc/src/lib.rs
+++ gc/src/lib.rs
@@ -2,19 +2,20 @@
 
 use std::{
     alloc::Layout,
-    cell::OnceCell,
-    collections::{BTreeSet, HashMap},
+    cell::{OnceCell, RefCell},
+    collections::{BTreeSet, HashMap, HashSet},
     fmt::Debug,
     marker::PhantomData,
     mem::MaybeUninit,
     num::NonZeroUsize,
     ops::{Deref, DerefMut},
     ptr::{addr_of, NonNull},
-    sync::{Mutex, Once, OnceLock},
+    sync::{atomic::AtomicBool, Mutex, Once, OnceLock},
     thread::JoinHandle,
     time::Duration,
 };
 
+mod alloc_store;
 mod global_gc;
 
 /// Makes sure the global garbage collector is initialized, and initializes it if is isn't
@@ -28,16 +29,27 @@ pub fn force_collect() {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct AllocAddr(pub NonZeroUsize);
+struct AllocAddr(NonZeroUsize);
+
+impl<T: ?Sized + GcAble> From<*const GcBox<T>> for AllocAddr {
+    fn from(value: *const GcBox<T>) -> Self {
+        Self((value as *const () as usize).try_into().unwrap())
+    }
+}
+
+impl<T: ?Sized + GcAble> From<*mut GcBox<T>> for AllocAddr {
+    fn from(value: *mut GcBox<T>) -> Self {
+        Self((value as *mut () as usize).try_into().unwrap())
+    }
+}
 
 struct AllocInfo {
-    start: AllocAddr,
-    layout: Layout,
+    marked: bool,
 }
 
 /// Stores all the information about the GC
 struct GcAlloc {
-    first_alloc: Option<NonNull<GcBox<dyn GcAble>>>,
+    allocs: HashMap<AllocAddr, NonNull<GcBox<dyn GcAble>>>,
     collection_handle: JoinHandle<()>,
 }
 
@@ -52,110 +64,55 @@ impl GcAlloc {
     }
     pub fn new() -> Self {
         GcAlloc {
-            first_alloc: None,
+            allocs: HashMap::new(),
             collection_handle: std::thread::spawn(|| Self::collection_loop()),
-        }
-    }
-
-    fn for_each_alloc(&self, f: impl Fn(*const GcBox<dyn GcAble>)) {
-        let mut this = self.first_alloc.as_ref();
-        loop {
-            match this {
-                Some(this_gcb) => {
-                    let this_gcb = this_gcb.as_ptr().cast_const();
-                    f(this_gcb);
-                    let next_value_of_this = GcBoxHeader::next_allocation(GcBox::header(this_gcb));
-                    this = unsafe { (*next_value_of_this).as_ref() };
-                }
-                None => return,
-            }
         }
     }
 
     /// Gives this `GcAlloc` control over the given `GcBox`, which is needed for it to be collected
     pub fn register_gcbox<T: Sized + GcAble>(&mut self, gcb: &mut GcBox<T>) {
-        if gcb.header.next_allocation.is_some() {
-            panic!(
-                "Called `register_gcbox` on a `GcBox<{}>` with a filled `next_allocation` field",
-                std::any::type_name::<T>()
-            )
-        }
-        gcb.header.next_allocation = self.first_alloc;
-        let gcb = NonNull::new(gcb as &mut GcBox<dyn GcAble>).unwrap();
-        self.first_alloc = Some(gcb);
+        let addr = AllocAddr::from(gcb as *mut _);
     }
 
     /// Mark then sweep
     pub fn mark_sweep(&mut self) {
         // Unmark all
-        // There's no need to recurse here
-        self.for_each_alloc(|alloc| {
-            let header = GcBox::header(alloc);
-            unsafe { &*header }.unmark()
-        });
-        // Mark all from stack
-        // Recurse to mark all downstream values
-        self.for_each_alloc(|alloc| {
-            let alloc = unsafe { alloc.as_ref() }.unwrap();
-            if alloc.header.is_rooted() {
-                alloc.header.mark();
-                unsafe { alloc.val.mark() };
-            }
-        });
-
-        /// Removes the allocation `dropping` and updates the linked list
-        ///
-        /// SAFETY:
-        /// `dropping` must be pointed to by `pointer_to_dropping`
-        unsafe fn remove_allocation(
-            pointer_to_dropping: &mut Option<NonNull<GcBox<dyn GcAble>>>,
-            dropping: &mut GcBox<dyn GcAble>,
-        ) {
-            // Update linked list
-            let pointer_to_after_dropping = dropping.header.next_allocation.clone();
-            *pointer_to_dropping = pointer_to_after_dropping;
-            // Drop and Deallocate
-            let b = unsafe { Box::from_raw(dropping) };
-            std::mem::drop(b);
+        for (_, nn) in &self.allocs {
+            let gcb = unsafe { nn.as_ref() };
+            gcb.header.unmark()
         }
 
-        // Cleanup unmarked boxes
-        let mut ptr_to_next = &mut self.first_alloc;
-        loop {
-            loop {
-                let Some(next) = ptr_to_next.as_mut() else {
-                    break;
-                };
-
-                let next = unsafe { next.as_mut() };
-
-                if !next.header.marked() {
-                    // drop...
-                    // deallocate...
-                    // update the linked list...
-                    unsafe { remove_allocation(ptr_to_next, next) };
-                }
-            }
-            match ptr_to_next {
-                Some(next) => ptr_to_next = &mut unsafe { next.as_mut() }.header.next_allocation,
-                None => break,
+        // Mark from stack
+        for (_, nn) in &self.allocs {
+            let gcb = unsafe { nn.as_ref() };
+            if gcb.header.is_rooted() {
+                gcb.header.mark();
+                unsafe { gcb.val.mark() }
             }
         }
+
+        // Deallocate & Drop unmarked
+        self.allocs.retain(|_, nn| {
+            let ptr = unsafe { nn.as_mut() };
+            let to_drop = !ptr.header.marked();
+            if to_drop {
+                // Drop & deallocate
+                Box::leak(unsafe { Box::from_raw(ptr) });
+                *nn = NonNull::<GcBox<()>>::dangling();
+            }
+
+            !to_drop
+        });
     }
 }
 
 pub(crate) struct GcBoxHeader {
     /// `true` -> This is referenced (indirectly or not) by a stack `Gc<_>`
-    marked: Mutex<bool>,
     root_count: Mutex<u32>,
-    next_allocation: Option<NonNull<GcBox<dyn GcAble>>>,
+    marked: Mutex<bool>,
 }
 
 impl GcBoxHeader {
-    fn next_allocation(this: *const Self) -> *const Option<NonNull<GcBox<dyn GcAble>>> {
-        unsafe { addr_of!((*this).next_allocation) }
-    }
-
     /// Returns true if this has a root count of more than 0
     pub fn is_rooted(&self) -> bool {
         *self.root_count.lock().unwrap() > 0
@@ -163,13 +120,11 @@ impl GcBoxHeader {
     pub fn marked(&self) -> bool {
         *self.marked.lock().unwrap()
     }
-    /// Not recursive
-    pub fn mark(&self) {
-        *self.marked.lock().unwrap() = true;
-    }
-    /// Not recursive
     pub fn unmark(&self) {
         *self.marked.lock().unwrap() = false;
+    }
+    pub fn mark(&self) {
+        *self.marked.lock().unwrap() = true;
     }
 }
 
@@ -235,7 +190,6 @@ impl<T: GcAble> Gc<T> {
             header: GcBoxHeader {
                 marked: Mutex::new(false),
                 root_count: Mutex::new(1), // < `1` since we are creating the first Gc here
-                next_allocation: None,
             },
             val,
         }));
@@ -342,14 +296,30 @@ pub unsafe trait GcAble: Send + Sync + 'static {
     unsafe fn set_not_root(&self);
 }
 
-// unsafe impl GcAble for i8 {}
-// unsafe impl GcAble for i16 {}
-// unsafe impl GcAble for i32 {}
-// unsafe impl GcAble for i64 {}
-// unsafe impl GcAble for i128 {}
+macro_rules! impl_gc_no_children {
+    ($t:ty) => {
+        unsafe impl GcAble for $t {
+            unsafe fn mark(&self) {}
 
-// unsafe impl GcAble for u8 {}
-// unsafe impl GcAble for u16 {}
-// unsafe impl GcAble for u32 {}
-// unsafe impl GcAble for u64 {}
-// unsafe impl GcAble for u128 {}
+            unsafe fn inc_root_count(&self) {}
+
+            unsafe fn dec_root_count(&self) {}
+
+            unsafe fn set_not_root(&self) {}
+        }
+    };
+}
+
+impl_gc_no_children!(());
+
+impl_gc_no_children!(i8);
+impl_gc_no_children!(i16);
+impl_gc_no_children!(i32);
+impl_gc_no_children!(i64);
+impl_gc_no_children!(i128);
+
+impl_gc_no_children!(u8);
+impl_gc_no_children!(u16);
+impl_gc_no_children!(u32);
+impl_gc_no_children!(u64);
+impl_gc_no_children!(u128);
